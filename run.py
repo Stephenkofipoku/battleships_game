15fe17190f38a83efc02f5a0113dffd3bce3ac5b
Stephenkofipoku <>
2023-10-10 04:51:01 +0000

    Add Main Baord Class

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 from random import randint
 
 scores = {"computer": 0, "player": 0}
+
+
+class Board:
+    """
+    Main board class. Sets board size, the name of the ships,
+    the player's name and the board type (player board or computer board).
+    Methods for adding ships, guesses and printing board.
+    """
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
